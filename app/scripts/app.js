d8e9b35d4df4d6d3a61bf697bbbd165a9af371de
Thompson B. Crozier <cybertrebuchet@gmail.com>
2015-10-02 18:47:15 -0400

    firebase is being a pain. gonna try https://thinkster.io/learn-to-build-realtime-webapps next

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -16,7 +16,8 @@ angular
     'ngRoute',
     'ngSanitize',
     'ngTouch',
-    'ui.sortable'
+    'ui.sortable',
+    'firebase'
   ])
   .config(function ($routeProvider) {
     $routeProvider
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,9 @@
  * Controller of the angularFirebaseForOmApp
  */
 angular.module('forOM')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $firebaseObject) {
+    var ref = new Firebase("https://crackling-fire-8466.firebaseio.com/whee");
+    $scope.data = $firebaseObject(ref);
     $scope.todos = ['Item 1', 'Item 2', 'Item 3'];
     $scope.addTodo = function () {
       $scope.todos.push($scope.todo);
